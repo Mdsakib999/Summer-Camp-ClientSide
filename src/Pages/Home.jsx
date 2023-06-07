c16e3fe5afb1a8696fe8b6c83458a71e00b6978a
Mdsakib999 <sakibabrar999@gmail.com>
2023-06-07 22:17:21 +0600

    banner section changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Bannar from '../components/Bannar';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Bannar></Bannar>
         </div>
     );
 };
---
+++ src/components/Bannar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+
+const Bannar = () => {
+    return (
+        <Carousel>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/banner-summerCamp-2_1200x500.jpg" />
+            
+        </div>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/banner-football_1200x500.jpg" />
+            
+        </div>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/Banner-Swim_1_1200x500.jpg" />
+            
+        </div>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/banner-crickte_1200x500.jpg" />
+            
+        </div>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/basketball-summer-camp_1200x500.jpg" />
+            <p className="legend">Legend 3</p>
+        </div>
+        <div>
+            <img src="https://www.simpleimageresizer.com/_uploads/photos/e047f9f2/banner-summerCamp_1200x500.jpeg" />
+            
+        </div>
+    </Carousel>
+    );
+};
+
+export default Bannar;
