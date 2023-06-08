344ea318a93a8ab5a7c9040d9b42fbeaab8dd0de
Mdsakib999 <sakibabrar999@gmail.com>
2023-06-08 11:22:20 +0600

    added title section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 import animation from "../assets/64644-fun-with-sports-box.json";
 import Lottie from "lottie-react";
+import Title from "./Title";
 
 const About = () => {
-    return (
-        <div className="rounded-lg shadow-md py-10 my-16 lg:flex justify-evenly items-center bg-[url('https://www.simpleimageresizer.com/_uploads/photos/676abd94/gradient-background-png-transparent-png-100_900x500.png')] bg-cover bg-no-repeat">
-        
+  return (
+    <section>
+      <Title
+      heading={"About US"}
+      subHeading={"why should join us"}
+      ></Title>
+      <div className="rounded-lg shadow-md py-10 mb-10 lg:flex justify-evenly items-center bg-[url('https://www.simpleimageresizer.com/_uploads/photos/676abd94/gradient-background-png-transparent-png-100_900x500.png')] bg-cover bg-no-repeat">
         <div>
           <Lottie animationData={animation} loop={true}></Lottie>
         </div>
@@ -13,11 +18,14 @@ const About = () => {
           <h1 className="lg:text-5xl font-semibold mb-4 leading-tight">
             Welcome to
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 font-bold">
-              RoboKingdom, 
+              RoboKingdom,
             </span>
             The Ultimate Robot Emporium!!!
           </h1>
-          <p className="text-4xl mb-4"> Discover the Perfect Toy & Shop Now!!</p>
+          <p className="text-4xl mb-4">
+            {" "}
+            Discover the Perfect Toy & Shop Now!!
+          </p>
           <p className="text-slate-500 hover:text-black">
             "Welcome to the Ultimate Robot Emporium! Unleash Boundless
             Excitement with Our Spectacular Collection of Robotic Marvels.
@@ -26,7 +34,8 @@ const About = () => {
           </p>
         </div>
       </div>
-    );
+    </section>
+  );
 };
 
 export default About;
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = ({heading, subHeading}) => {
+    return (
+        <div className='w-3/12 text-center mx-auto my-10'>
+            <h1 className='text-4xl font-bold border-b-4 pb-3'>{heading}</h1>
+            <p className='text-xl text-slate-500 pt-3'>---{subHeading}---</p>
+        </div>
+    );
+};
+
+export default Title;
